020b68480f55854aace2dee3e6348ff88f9f03e8
Art P. <fn3x@yandex.com>
2025-01-08 07:05:19 +0400

    fix(shell): fixed npm global packages path

--- shells/work/flake.nix
+++ shells/work/flake.nix
@@ -9,11 +9,13 @@
     { self, nixpkgs }:
     let
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      NPM_CONFIG_PREFIX = toString ./npm_config_prefix;
     in
     {
       devShells.x86_64-linux.work = pkgs.mkShell {
         packages = with pkgs; [
           nodejs_23
+          pnpm
           go_1_23
           awscli2
           protobuf
@@ -21,5 +23,10 @@
           protoc-gen-js
         ];
       };
+
+      inherit NPM_CONFIG_PREFIX;
+      shellHook = ''
+        export PATH="${NPM_CONFIG_PREFIX}/bin:$PATH"
+      '';
     };
 }
