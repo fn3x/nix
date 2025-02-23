cef5cbead74478884b8501050838687631f46d1d
Art P. <fn3x@yandex.ru>
2025-02-23 05:32:36 +0400

    feat: added gstreamer; updated dependencies

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -148,6 +148,12 @@ in
     wireguard-tools 
     libva
     mangohud
+    gst_all_1.gstreamer
+    gst_all_1.gst-plugins-base
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-vaapi 
   ];
 
   services.resolved.enable = true;
