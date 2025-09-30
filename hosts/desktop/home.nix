85eebe0df1f31902bf3e65020794f13837964842
Art P. <fn3x@yandex.ru>
2025-09-30 13:53:27 +0400

    fix(hyprland): fix for deprecated window rule

--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -842,7 +842,7 @@ BOOT_TIMEOUT="120"
       # See https://wiki.hyprland.org/Configuring/Workspace-Rules/ for workspace rules
 
       # Ignore maximize requests from apps. You'll probably like this.
-      windowrulev2 = suppressevent maximize decorate:0, class:.*
+      windowrulev2 = suppressevent maximize, class:.*
 
       # Fix some dragging issues with XWayland
       windowrulev2 = nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0
