b9084e5ee5a0cf1473c0c9da01b88dd0e3214fa3
Art P. <fn3x@yandex.ru>
2025-12-13 07:42:43 +0400

    feat(teamspeak): updated to 6.0.0-beta3.4

--- modules/programs/teamspeak/teamspeak-client.nix
+++ modules/programs/teamspeak/teamspeak-client.nix
@@ -4,11 +4,11 @@
 }:
 pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "teamspeak6-client";
-  version = "6.0.0-beta3.3";
+  version = "6.0.0-beta3.4";
 
   src = pkgs.fetchurl {
     url = "https://files.teamspeak-services.com/pre_releases/client/${finalAttrs.version}/teamspeak-client.tar.gz";
-    hash = "sha256-ElPfy3A/wazuEkB1D2UuoijLMpKfQTmKc1FRfgWh8po=";
+    hash = "sha256-ubpAigtYFwzjI4T8i7pWgAhA1pS9MQBQy63QkkbUvyc=";
   };
 
   sourceRoot = ".";
